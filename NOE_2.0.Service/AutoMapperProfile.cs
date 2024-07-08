44982ec4321837a8de66ae89e45e0075fe8478e0
Ariel Malerba <arielmalerba84@hotmail.com>
2024-07-08 15:29:12 -0300

    tercer commit

---
+++ NOE_2.0.Service/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NUEVO.NOE.Model.Seguridad;
+
+namespace NUEVO.NOE.Service
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Rol, RolDTO>().ReverseMap();
+            CreateMap<UsuarioRol, UsuarioRolDTO>().ReverseMap();
+        }
+    }
+}
